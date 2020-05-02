f1236277930c282d17abf72c1bd17a40d06fd33d
Minewine <Mtcoughlan@gmail.com>
2020-05-02 10:25:40 +0200

    test

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='gittest',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='mick',
+    author_email='',
+    description=''
+)
